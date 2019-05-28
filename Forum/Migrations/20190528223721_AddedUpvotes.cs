df6423d82a29c374886754d7b203f57682c2df53
Vs Ritwik <ritz.rag13@gmail.com>
2019-05-28 18:38:29 -0400

    updated the databse

---
+++ Forum/Migrations/20190528223721_AddedUpvotes.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Forum.Migrations
+{
+    public partial class AddedUpvotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Upvotes",
+                table: "Post",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PostID",
+                table: "Comment",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OPID",
+                table: "Comment",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Upvotes",
+                table: "Comment",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Upvotes",
+                table: "Post");
+
+            migrationBuilder.DropColumn(
+                name: "Upvotes",
+                table: "Comment");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PostID",
+                table: "Comment",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OPID",
+                table: "Comment",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
